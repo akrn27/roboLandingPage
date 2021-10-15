06a214b391855bf39a4128eb4625cfb991e5f5fa
Akrn27 <akurnia755@gmail.com>
2021-10-15 17:22:17 +0700

    add Grid.js components

---
+++ src/components/Grid.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/styles';
+import CustomBtn from "./CustomBtn";
+
+const styles = makeStyles({
+    wrapper: {
+        display: 'flex',
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "0 5rem 0 5rem"
+    },
+    item: {
+        paddingTop: "1rem"
+    }
+})
+
+function Grid() {
+    return (
+
+    )
+}
+
+export default Grid;
