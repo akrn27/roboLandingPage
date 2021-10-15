19ada12040d85bb6e505d1254566bf60df530e94
Akrn27 <akurnia755@gmail.com>
2021-10-16 03:22:38 +0700

    finish mockup Footer.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {ThemeProvider, makeStyles} from '@material-ui/styles'
 import {Typography} from '@material-ui/core'
 import Navbar from './components/Navbar.js'
 import Grid from './components/Grid'
+import Footer from './components/Footer.js';
 import './App.css';
 
 import SecurityIcon from '@material-ui/icons/Security';
@@ -80,6 +81,9 @@ function App() {
           <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
           <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
           <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer />
         </div>
       </ThemeProvider>
     </div>
